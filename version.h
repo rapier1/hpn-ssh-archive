3224454b616d9708c3bc88559a28238213ecc738
Chris Rapier <rapier@psc.edu>
2013-10-07 14:07:20 -0400

    fixed unnecessary debug in sshd.c, whitespace in version.h, wrong buf size in channels.h

--- version.h
+++ version.h
@@ -3,5 +3,5 @@
 #define SSH_VERSION	"OpenSSH_6.2"
 
 #define SSH_PORTABLE	"p2"
-#define SSH_HPN                "-hpn14v1"
+#define SSH_HPN         "-hpn14v1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
