d8033e62ddd285c3108139a556f88eab720c0788
Chris Rapier <rapier@psc.edu>
2013-10-07 15:24:15 -0400

    Updated version.h to correct HPN revision

--- version.h
+++ version.h
@@ -3,5 +3,5 @@
 #define SSH_VERSION	"OpenSSH_6.3"
 
 #define SSH_PORTABLE	"p1"
-#define SSH_HPN         "-hpn14v1"
+#define SSH_HPN         "-hpn14v2"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE SSH_HPN
